3abcc431910bda51b3ada131a8b300d9282bdbf3
wzurawski015 <watwzwp@gmail.com>
2026-01-15 17:50:03 +0100

    App: Integrate UART service into demo_lcd_rgb

--- firmware/projects/demo_lcd_rgb/main/main.c
+++ firmware/projects/demo_lcd_rgb/main/main.c
@@ -6,9 +6,10 @@
 
 #include "services_timer.h"
 #include "services_i2c.h"
+#include "services_uart.h"   // +++
 
-#include "app_log_bus.h"     // log -> EV (STREAM/READY)
-#include "app_demo_lcd.h"    // aktor LCD
+#include "app_log_bus.h"
+#include "app_demo_lcd.h"
 
 #if CONFIG_INFRA_LOG_CLI
 #include "logging_cli.h"
@@ -20,31 +21,40 @@ static void set_default_log_levels(void)
     esp_log_level_set("APP",     ESP_LOG_WARN);
     esp_log_level_set("LOGCLI",  ESP_LOG_INFO);
     esp_log_level_set("DFR_LCD", ESP_LOG_DEBUG);
-    esp_log_level_set("APP_DEMO_LCD", ESP_LOG_INFO);
+    esp_log_level_set("SVC_UART", ESP_LOG_INFO); // +++
 }
 
 void app_main(void)
 {
-    // 0) Poziomy logów – ergonomia od pierwszego bootu
     set_default_log_levels();
 
-    // 1) Rdzeń: event-bus + pula buforów (lease)
     ev_init();
     lp_init();
 
     const ev_bus_t* bus = ev_bus_default();
 
-    // 2) Serwisy systemowe
+    // 1. Timery i I2C
     services_timer_start(bus);
     services_i2c_start(bus, 16, 4096, 8);
 
-    // 3) Most log->EV (STREAM): każda nowa linia logu = EV_LOG_READY, payload w ringu
+    // 2. Logi
     app_log_bus_start(bus);
 
-    // 4) Aplikacja jako aktor (LCD, w razie potrzeby inne)
+    // 3. UART Service (M2M) na UART1
+    //    Uwaga: Piny 4/5 dla C6 (przykładowo), dostosuj do płytki!
+    uart_svc_cfg_t ucfg = {
+        .uart_num = 1,
+        .tx_pin = 4,
+        .rx_pin = 5,
+        .baud_rate = 115200,
+        .pattern_char = '\n' // Wykrywaj koniec linii
+    };
+    services_uart_start(bus, &ucfg);
+
+    // 4. Aktorzy
     app_demo_lcd_start(bus);
 
-    // 5) CLI (opcjonalnie)
+    // 5. CLI
 #if CONFIG_INFRA_LOG_CLI
   #if CONFIG_INFRA_LOG_CLI_START_REPL
     infra_log_cli_start_repl();
@@ -53,6 +63,5 @@ void app_main(void)
   #endif
 #endif
 
-    // 6) Main kończy się – żyją aktorzy/serwisy
     vTaskDelete(NULL);
 }
